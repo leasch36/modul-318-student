ba84b6c3714200a2e84fa30b2fe89a5fc5d06a52
Lea Schoepfer <Lea_Schoepfer@sluz.ch>
2020-12-15 10:48:08 +0100

    test

--- MyTransportApp/Form1.Designer.cs
+++ MyTransportApp/Form1.Designer.cs
@@ -29,13 +29,270 @@ namespace MyTransportApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.SearchForConnectionTabPage = new System.Windows.Forms.TabPage();
+            this.DepartureBoardTabPage = new System.Windows.Forms.TabPage();
+            this.MapTabPage = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.TabControl.SuspendLayout();
+            this.SearchForConnectionTabPage.SuspendLayout();
+            this.DepartureBoardTabPage.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ÖV-Suche";
+            // 
+            // TabControl
+            // 
+            this.TabControl.Controls.Add(this.SearchForConnectionTabPage);
+            this.TabControl.Controls.Add(this.DepartureBoardTabPage);
+            this.TabControl.Controls.Add(this.MapTabPage);
+            this.TabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TabControl.Location = new System.Drawing.Point(12, 55);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(661, 500);
+            this.TabControl.TabIndex = 2;
+            // 
+            // SearchForConnectionTabPage
+            // 
+            this.SearchForConnectionTabPage.Controls.Add(this.label2);
+            this.SearchForConnectionTabPage.Controls.Add(this.label3);
+            this.SearchForConnectionTabPage.Controls.Add(this.listView1);
+            this.SearchForConnectionTabPage.Controls.Add(this.groupBox2);
+            this.SearchForConnectionTabPage.Controls.Add(this.comboBox1);
+            this.SearchForConnectionTabPage.Controls.Add(this.comboBox2);
+            this.SearchForConnectionTabPage.Controls.Add(this.button1);
+            this.SearchForConnectionTabPage.Location = new System.Drawing.Point(4, 25);
+            this.SearchForConnectionTabPage.Name = "SearchForConnectionTabPage";
+            this.SearchForConnectionTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.SearchForConnectionTabPage.Size = new System.Drawing.Size(653, 471);
+            this.SearchForConnectionTabPage.TabIndex = 0;
+            this.SearchForConnectionTabPage.Text = "Verbindungssuche";
+            this.SearchForConnectionTabPage.UseVisualStyleBackColor = true;
+            // 
+            // DepartureBoardTabPage
+            // 
+            this.DepartureBoardTabPage.Controls.Add(this.listView2);
+            this.DepartureBoardTabPage.Controls.Add(this.button2);
+            this.DepartureBoardTabPage.Controls.Add(this.comboBox3);
+            this.DepartureBoardTabPage.Controls.Add(this.label6);
+            this.DepartureBoardTabPage.Location = new System.Drawing.Point(4, 25);
+            this.DepartureBoardTabPage.Name = "DepartureBoardTabPage";
+            this.DepartureBoardTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.DepartureBoardTabPage.Size = new System.Drawing.Size(653, 471);
+            this.DepartureBoardTabPage.TabIndex = 1;
+            this.DepartureBoardTabPage.Text = "Abfahrtstafel";
+            this.DepartureBoardTabPage.UseVisualStyleBackColor = true;
+            // 
+            // MapTabPage
+            // 
+            this.MapTabPage.Location = new System.Drawing.Point(4, 25);
+            this.MapTabPage.Name = "MapTabPage";
+            this.MapTabPage.Size = new System.Drawing.Size(653, 471);
+            this.MapTabPage.TabIndex = 2;
+            this.MapTabPage.Text = "Karte";
+            this.MapTabPage.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Location = new System.Drawing.Point(6, 73);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(417, 57);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 147);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(653, 324);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(412, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(59, 22);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(265, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "label5";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(317, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(85, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "label6";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(26, 40);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(172, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(258, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(65, 29);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(206, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(6, 86);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(641, 358);
+            this.listView2.TabIndex = 3;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(685, 564);
+            this.Controls.Add(this.TabControl);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TabControl.ResumeLayout(false);
+            this.SearchForConnectionTabPage.ResumeLayout(false);
+            this.SearchForConnectionTabPage.PerformLayout();
+            this.DepartureBoardTabPage.ResumeLayout(false);
+            this.DepartureBoardTabPage.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl TabControl;
+        private System.Windows.Forms.TabPage SearchForConnectionTabPage;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage DepartureBoardTabPage;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TabPage MapTabPage;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ListView listView2;
     }
 }
 
--- MyTransportApp/Form1.cs
+++ MyTransportApp/Form1.cs
@@ -16,5 +16,10 @@ namespace MyTransportApp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
